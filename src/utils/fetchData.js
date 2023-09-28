98d6bfe99907d32206b66ffca5ee7d4a2398fd25
khoapham23112004 <tankhoapham2311@gmail.com>
2023-09-28 14:49:44 +0700

    change server URL

--- src/utils/fetchData.js
+++ src/utils/fetchData.js
@@ -3,10 +3,11 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 export const apiService = axios.create({
-  baseURL: process.env.SERVER_URL || "http://localhost:5000",
+  baseURL:
+    process.env.SERVER_URL || "https://backend-final-project-fcdf.onrender.com",
   withCredentials: true,
 });
-//https://backend-final-project-fcdf.onrender.com
+//
 export const getDataAPI = async (url, token) => {
   const res = await apiService.get(`/api/${url}`, {
     headers: { Authorization: token },
